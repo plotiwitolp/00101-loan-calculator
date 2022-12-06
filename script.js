d965cca8cab60b55927994fadadb6841d311d269
plotiwitolp <plotiwitolp@gmail.com>
2022-12-07 00:13:35 +0400

    update js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
   $('.loan-calculator__item-calculate').on('click', calc);
   $('.loan-calculator__item-reset').on('click', reset);
+
+  let loanAmountMin = $('.loan-calculator__loan-amount').attr('min');
+  let loanAmountMax = $('.loan-calculator__loan-amount').attr('max');
+  let monthsMin = $('.loan-calculator__months').attr('min');
+  let monthsMax = $('.loan-calculator__months').attr('max');
+
   let result = 0;
   function calc() {
     let rate = $('.loan-calculator__rate').val();
     let loanAmount = $('.loan-calculator__loan-amount').val();
     let months = $('.loan-calculator__months').val();
     result = rate * loanAmount + loanAmount / months;
-    if (Number.isInteger(result)) {
+
+    if (Number.isInteger(result) & (+loanAmount >= +loanAmountMin) & (+loanAmount <= +loanAmountMax) & (+months >= +monthsMin) & (+months <= +monthsMax)) {
       $('.loan-calculator__err').hide();
     } else {
       $('.loan-calculator__err').show();
@@ -15,6 +22,7 @@ $(document).ready(function () {
 
     $('.loan-calculator__result').val(result);
   }
+
   function reset() {
     $('.loan-calculator__rate').val(0.007);
     $('.loan-calculator__loan-amount').val(100000);
